81f844e48fe5f58094a23b5a8c852c9adef27f84
An Le <an.le@i-on.net>
2023-09-11 21:14:31 +0700

    random animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,15 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({subsets: ['latin']})
 
+const RANDOM_DISPLAY = '220453386475668369126874203127722456847118518118905049373063640445614984272665739406245400974402082908210402446817910019961841220966268695509083924551702897199045677643021929965748824550251725709767717047663154169322883122638800682673526663456697904317695347748041774350911325407657575706056671092101'
+
 export default function Home() {
   const [randomNumber, setRandomNumber] = useState(null);
   const [history, setHistory] = useState([]);
+  const [pending, setPending] = useState(false);
 
   const generateNumber = () => {
+    setPending(true);
     let random = 0;
     while (random === 0) {
       random = Math.floor(Math.random() * 500);
@@ -17,8 +21,12 @@ export default function Home() {
     const result = Array(Math.max(3 - random.toString().length, 0)).fill(0)
     result.push(random);
     const resultStr = result.join('');
-    setRandomNumber(resultStr);
-    setHistory((prev) => [...prev, resultStr])
+
+    setTimeout(() => {
+      setPending(false)
+      setRandomNumber(resultStr);
+      setHistory((prev) => [{result: resultStr, time: (new Date()).toLocaleTimeString()}, ...prev])
+    }, 5000);
   }
 
   return (
@@ -32,14 +40,19 @@ export default function Home() {
       <main className={`${styles.container} ${inter.className}`}>
         <div className={styles.box}>
           <h1 className={styles.mainTitle}>Bốc thăm trúng thưởng</h1>
-          <div className={styles.result}>
-            {randomNumber}
+          <div className={styles.resultWrapper}>
+            <div className={`${styles.result} ${pending ? 'pending' : ''}`}>
+              {pending ? RANDOM_DISPLAY : randomNumber}
+            </div>
           </div>
           <button className={styles.eventButton} onClick={generateNumber}>Quay Số</button>
           <h2 className={styles.historyTitle}>Danh sách trúng thưởng</h2>
-          <div className={styles.historyArea}>
-            {history.map((item) => (<p id={item}>{item}</p>))}
-          </div>
+          <ul className={styles.historyArea}>
+            {history.map((item) => (<li key={item.result}>
+              <strong>{item.result}</strong>
+              <span>{item.time}</span>
+            </li>))}
+          </ul>
         </div>
       </main>
     </>
